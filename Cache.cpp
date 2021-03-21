b3000fd6afca30102514ffe846382e93becc61c8
SamuelMosesA <asamuelmoses@outlook.com>
2021-03-21 17:00:27 +0530

    Wrote Pseudo LRU code

--- Cache.cpp
+++ Cache.cpp
@@ -1,62 +1,150 @@
 #include<iostream>
 #include<fstream>
 #include<string.h>
+#include <vector>
 
-using namespace std;;
+using namespace std;
 
-class Cache{
-	string **cache;
-	long capacity;
-	long sets;
-	long size;
-	int ways;
-	int replacementPolicy;
-	long block;
-	public:
-	Cache();
-	~Cache();
-	void load(string address);
+class PLRUTree {
+
+    int n, k; //n = 2^k -1 elements
+    vector<bool> tree;
+
+    static int left(int i) {
+        return 2 * i + 1;
+    }
+
+    static int right(int i) {
+        return 2 * i + 2;
+    }
+
+
+public:
+    PLRUTree(int assc, int k);
+
+    int replace();
+
+    void read(int ind);
 };
 
-Cache::Cache()
-{
-	cin>>size>>block>>ways>>replacementPolicy;
-	capacity = size/block;
-	if(ways==0)
-	{
-		sets = 1;
-		cache = new string*[1];
-		cache[0] = new string[ways];
-	}
-	else
-	{
-		sets = capacity/ways;
-		cache = new string*[sets];
-		for(int i=0; i<sets; i++)
-			cache[i]=new string[ways];
-	}
+PLRUTree::PLRUTree(int assc, int k) {
+    n = assc - 1;
+    this->k = k;
+    tree.resize(n, 0);
 }
 
-Cache::~Cache()
-{
-	for(int i=0; i<sets; i++)
-		delete[] cache[i];
-	delete[] cache;
+int PLRUTree::replace() {
+    int i = 0;
+    while (i < n) {
+        bool bit = tree[i];
+        tree[i] = !bit;
+        if (bit) {
+            i = right(i);
+        } else {
+            i = left(i);
+        }
+    }
 
+    return i - n;
 }
 
-void Cache::load(string address)
-{
-	
+void PLRUTree::read(int ind) {
+    int i = 0;
+    for (int j = k; j >= 0; --j) {
+        bool bit = (ind & (1 << j)) > 0;
+        tree[i] = !bit;
+        if (bit)
+            i = right(i);
+        else
+            i = left(i);
+    }
 }
 
-int main(){
-	Cache c;
-	string inp;
-	cin>>inp;
-	ifstream addresses(inp);
-	while(getline (addresses, inp))
-		c.load(inp);
-	addresses.close();
-	return 0;
+
+class Cache {
+    vector<vector<string>> cache;
+    vector<PLRUTree> cacheTrees;
+    long capacity;
+    long sets;
+    long size;
+    int ways;
+    int replacementPolicy;
+    long block;
+public:
+    Cache();
+
+    ~Cache();
+
+    void read(string address);
+
+    void write(string address);
+
+    int searchTagArray(int ind, int tag);
+};
+
+Cache::Cache() {
+    cin >> size >> block >> ways >> replacementPolicy;
+    capacity = size / block;
+    if (ways == 0) {
+        sets = 1;
+        cache.resize(1);
+        cache[0].resize(ways);
+    } else {
+        sets = capacity / ways;
+        cache.resize(sets);
+        for (int i = 0; i < sets; i++) {
+            cache[i].resize(ways);
+        }
+    }
+    if (ways >= 2 && replacementPolicy == 2) {
+        int k = 0;
+        while ((ways & (1 << k))==0)
+            ++k;
+        cacheTrees.resize(sets, PLRUTree(ways, k));
+    }
+}
+
+
+void Cache::read(string address) {
+    if (replacementPolicy == 2) {
+        int ind=0, tag = 0; //from address;
+        int res = searchTagArray(ind, tag);
+        if (res != -1) {
+            cacheTrees[ind].read(res);
+            //increment the counters
+        }else{
+            cacheTrees[ind].replace();
+            //increment the counters
+        }
+    }
+}
+
+void Cache::write(string address) {
+    if (replacementPolicy == 2) {
+        int ind=0, tag = 0; //from address;
+        int res = searchTagArray(ind, tag);
+        if (res != -1) {
+            cacheTrees[ind].read(res);
+            //increment the counters
+        }else{
+            cacheTrees[ind].replace();
+            //increment the counters
+        }
+    }
+    //dirty bit
+}
+
+int Cache::searchTagArray(int ind, int tag) {
+    return 0;
+}
+
+int main() {
+    Cache c;
+    string inp;
+    cin >> inp;
+    ifstream addresses(inp);
+    while (getline(addresses, inp))
+        c.read(inp);
+    addresses.close();
+    return 0;
 }
